6b7b41ce2156d7ee0c4cd2b1dd3debb029ffffb4
PAOLO MUSOLINO <info@codeido.com>
2016-11-27 18:30:18 +0100

    Made view to send messages.

---
+++ CustomerlySDK/Library/Subclasses/CyButton.swift
@@ -0,0 +1,13 @@
+//
+//  CyButton.swift
+//  Customerly
+//
+//  Created by Paolo Musolino on 27/11/16.
+//  Copyright © 2016 Customerly. All rights reserved.
+//
+
+import UIKit
+
+class CyButton: UIButton {
+
+}
---
+++ CustomerlySDK/Library/Subclasses/CyTextField.swift
@@ -0,0 +1,13 @@
+//
+//  CyTextField.swift
+//  Customerly
+//
+//  Created by Paolo Musolino on 27/11/16.
+//  Copyright © 2016 Customerly. All rights reserved.
+//
+
+import UIKit
+
+class CyTextField: UITextField {
+
+}
