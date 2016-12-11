2e653ffa1d7cfe91f0dd410da53ca14e45eaeff0
PAOLO MUSOLINO <info@codeido.com>
2016-12-11 17:05:53 +0100

    fix vertical alignment inside message cell -> text view

--- CustomerlySDK/Library/View Controllers/Cells/CyMessageTableViewCell.swift
+++ CustomerlySDK/Library/View Controllers/Cells/CyMessageTableViewCell.swift
@@ -20,7 +20,7 @@ class CyMessageTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         messageView.layer.cornerRadius = 4.0
-        messageTextView.textContainerInset = UIEdgeInsetsMake(0, 0, -15, 0) //remove padding
+        messageTextView.textContainerInset = UIEdgeInsetsMake(2, 0, -15, 0) //remove padding
         adminAvatar.layer.cornerRadius = adminAvatar.frame.size.width/2 //Circular avatar for admin
         userAvatar.layer.cornerRadius = userAvatar.frame.size.width/2 //Circular avatar for user
         self.backgroundColor = UIColor.clear
