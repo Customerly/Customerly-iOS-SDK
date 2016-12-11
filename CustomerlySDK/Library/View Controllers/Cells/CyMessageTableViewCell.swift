975cba908601409861935f0eb73581c3c131e5d8
PAOLO MUSOLINO <info@codeido.com>
2016-12-11 18:18:48 +0100

    - Fix alignment messages on left or right side

--- CustomerlySDK/Library/View Controllers/Cells/CyMessageTableViewCell.swift
+++ CustomerlySDK/Library/View Controllers/Cells/CyMessageTableViewCell.swift
@@ -14,8 +14,8 @@ class CyMessageTableViewCell: UITableViewCell {
     @IBOutlet weak var userAvatar: CyImageView!
     @IBOutlet weak var messageView: UIView!
     @IBOutlet weak var messageTextView: CyTextView!
-    @IBOutlet weak var messageViewLeftConstraint: NSLayoutConstraint!
-    @IBOutlet weak var messageViewRightConstraint: NSLayoutConstraint!
+    @IBOutlet var messageViewLeftConstraint: NSLayoutConstraint!
+    @IBOutlet var messageViewRightConstraint: NSLayoutConstraint!
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -24,6 +24,11 @@ class CyMessageTableViewCell: UITableViewCell {
         adminAvatar.layer.cornerRadius = adminAvatar.frame.size.width/2 //Circular avatar for admin
         userAvatar.layer.cornerRadius = userAvatar.frame.size.width/2 //Circular avatar for user
         self.backgroundColor = UIColor.clear
+        
+        if let app_config = CyStorage.getCyDataModel()?.app_config{
+            messageView.backgroundColor = app_config.widget_color != nil ? UIColor(hexString: app_config.widget_color!) : base_color_template
+        }
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -35,11 +40,15 @@ class CyMessageTableViewCell: UITableViewCell {
         messageTextView.textAlignment = .left
         userAvatar.isHidden = true
         adminAvatar.isHidden = false
+        messageViewRightConstraint.isActive = false
+        messageViewLeftConstraint.isActive = true
     }
     
     func setUserVisual(){
         userAvatar.isHidden = false
         adminAvatar.isHidden = true
         messageTextView.textAlignment = .right
+        messageViewRightConstraint.isActive = true
+        messageViewLeftConstraint.isActive = false
     }
 }
