c590a2b6d5baaa5ad5e2eea2f263b62595ef3e61
PAOLO MUSOLINO <info@codeido.com>
2017-02-05 17:39:18 +0100

    Fix sound bundle search

--- CustomerlySDK/Library/Subclasses/CySound.swift
+++ CustomerlySDK/Library/Subclasses/CySound.swift
@@ -10,18 +10,40 @@
 import AudioToolbox
 
 class CySound: NSObject {
-
+    
     static func playNotification(){
+        
         let podBundle = Bundle(for: Customerly.classForCoder())
-        if let bundleURL = podBundle.url(forResource: "notification", withExtension: "m4r"){
-            var soundId: SystemSoundID = 0
-            AudioServicesCreateSystemSoundID(bundleURL as CFURL, &soundId)
+        
+        if let bundleURL = podBundle.url(forResource: "CustomerlySDK", withExtension: "bundle"){
             
-            AudioServicesAddSystemSoundCompletion(soundId, nil, nil, { (soundId, clientData) -> Void in
-                AudioServicesDisposeSystemSoundID(soundId)
-            }, nil)
+            if let bundle = Bundle(url: bundleURL) {
+                if let url = bundle.url(forResource: "notification", withExtension: "m4r"){
+                    CySound.playSoundFromFile(url: url)
+                    return
+                }
+            }
+            else {
+                assertionFailure("Could not load the bundle")
+            }
             
-            AudioServicesPlaySystemSound(soundId)
         }
+        
+        if let bundleURL = podBundle.url(forResource: "notification", withExtension: "m4r"){
+            CySound.playSoundFromFile(url: bundleURL)
+        }
+        
+        return
+    }
+    
+    static func playSoundFromFile(url: URL){
+        var soundId: SystemSoundID = 0
+        AudioServicesCreateSystemSoundID(url as CFURL, &soundId)
+        
+        AudioServicesAddSystemSoundCompletion(soundId, nil, nil, { (soundId, clientData) -> Void in
+            AudioServicesDisposeSystemSoundID(soundId)
+        }, nil)
+        
+        AudioServicesPlaySystemSound(soundId)
     }
 }
