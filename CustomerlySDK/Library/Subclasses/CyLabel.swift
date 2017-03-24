9b68315bc2e9e9d0a7633b88aa862223070e1d5c
PAOLO MUSOLINO <info@codeido.com>
2017-03-24 22:20:48 +0100

    - Now CyLabel support insets - The welcome message label now show background color (#555555), round corner and 8 of insets (top, bottom, left, right)

--- CustomerlySDK/Library/Subclasses/CyLabel.swift
+++ CustomerlySDK/Library/Subclasses/CyLabel.swift
@@ -10,5 +10,52 @@ import UIKit
 
 class CyLabel: UILabel {
 
-
+    @IBInspectable var borderColor: UIColor = UIColor.clear
+    @IBInspectable var borderWidth: CGFloat = 0
+    @IBInspectable var cornerRadius: CGFloat = 0
+    @IBInspectable var topInset: CGFloat = 0.0
+    @IBInspectable var bottomInset: CGFloat = 0.0
+    @IBInspectable var leftInset: CGFloat = 0.0
+    @IBInspectable var rightInset: CGFloat = 0.0
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.customize()
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.customize()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        customize()
+    }
+    
+    func customize(){
+        self.layer.masksToBounds = true
+        self.layer.borderColor = self.borderColor.cgColor
+        self.layer.borderWidth = self.borderWidth
+        self.layer.cornerRadius = self.cornerRadius
+    }
+    
+    override func drawText(in rect: CGRect) {
+        let insets = UIEdgeInsets(top: topInset, left: leftInset, bottom: bottomInset, right: rightInset)
+        super.drawText(in: UIEdgeInsetsInsetRect(rect, insets))
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        get {
+            var contentSize = super.intrinsicContentSize
+            contentSize.height += topInset + bottomInset
+            contentSize.width += leftInset + rightInset
+            return contentSize
+        }
+    }
+    
 }
