a648af83085728be427c998c1401bd2e4941008d
PAOLO MUSOLINO <info@codeido.com>
2017-05-16 22:31:02 +0200

    Fix space in code

--- CustomerlySDK/Library/View Controllers/Cells/CyConversationTableViewCell.swift
+++ CustomerlySDK/Library/View Controllers/Cells/CyConversationTableViewCell.swift
@@ -24,7 +24,6 @@ class CyConversationTableViewCell: UITableViewCell {
     
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-        
     }
     
     func unreadConversation(unread: Bool){
